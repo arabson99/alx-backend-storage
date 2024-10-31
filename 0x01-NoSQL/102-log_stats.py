445d1693a02770c851c31c2c5387d426a743e1e0
arabson99 <111277887+arabson99@users.noreply.github.com>
2024-10-31 16:35:30 +0100

    Update

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,40 +1,26 @@
 #!/usr/bin/env python3
-"""
-Log stats
-"""
+
 from pymongo import MongoClient
 
-
-def log_stats():
-    """ log_stats.
-    """
-    client = MongoClient('mongodb://127.0.0.1:27017')
+if __name__ == "__main__":
+    """Prints the log stats in nginx collection"""
+    client = MongoClient('mongodb://localhost:27017')
     logs_collection = client.logs.nginx
-    total = logs_collection.count_documents({})
-    get = logs_collection.count_documents({"method": "GET"})
-    post = logs_collection.count_documents({"method": "POST"})
-    put = logs_collection.count_documents({"method": "PUT"})
-    patch = logs_collection.count_documents({"method": "PATCH"})
-    delete = logs_collection.count_documents({"method": "DELETE"})
-    path = logs_collection.count_documents(
-        {"method": "GET", "path": "/status"})
-    print(f"{total} logs")
+
+    print(f"{logs_collection.estimated_document_count()} logs")
+
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     print("Methods:")
-    print(f"\tmethod GET: {get}")
-    print(f"\tmethod POST: {post}")
-    print(f"\tmethod PUT: {put}")
-    print(f"\tmethod PATCH: {patch}")
-    print(f"\tmethod DELETE: {delete}")
-    print(f"{path} status check")
+
+    for method in methods:
+        print("\tmethods {}: {}".format(method, logs_collection.count_documents({"method": method})))
+
+    print("{} status check".format(logs_collection.count_documents({"method": "GET", "path": "/status"})))
+
     print("IPs:")
-    sorted_ips = logs_collection.aggregate([
-        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-        {"$sort": {"count": -1}},
-        {"$limit": 10}
-    ])
+    sorted_ips = logs_collection.aggregate(
+            [{"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+                {"$sort": {"count": -1}}, {"$limit": 10}])
 
     for ip in sorted_ips:
         print(f"\t{ip.get('_id')}: {ip.get('count')}")
-
-if __name__ == "__main__":
-    log_stats()
