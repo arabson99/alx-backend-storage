fe7aed26f551ca3f85b1973f4914af03babaa9d2
arabson99 <111277887+arabson99@users.noreply.github.com>
2024-11-02 03:54:40 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,35 +1,38 @@
 #!/usr/bin/env python3
-""" A module for fetching and caching web pages. """
+'''A module with tools for request caching and tracking.
+'''
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
-# Initialize Redis client
-r = redis.Redis()
 
-def count_calls(method: Callable) -> Callable:
-    """ Decorator to count how many time a URL is accessed and cache the result."""
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
+
+
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
     @wraps(method)
-    def wrapper(url: str) -> str:
-        """ Wrapper that tracks access count and manages caching."""
-        r.incr(f"count:{url}")
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
 
-        cache_html = r.get(f"cache:{url}")
-        if cache_html:
-            return cache_html.decode('utf-8')
 
-        # call the original method to fetch the page cotent
-        html = method(url)
-        r.set(f"count:{url}", 0)
-        # cache the result with an expiration time of 10 sec
-        r.setex(f"cache:{url}", 10, html)
-        return html
-    return wrapper
-
-@count_calls
+@data_cacher
 def get_page(url: str) -> str:
-    """ Fetch the HTML content of a URL. """
-    request = requests.get(url)
-    request.raise_for_status()
-    return request.text
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
